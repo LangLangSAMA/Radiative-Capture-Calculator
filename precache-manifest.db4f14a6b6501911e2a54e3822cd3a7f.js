fe0d1b72d6db9c10ce07e76164fb2b9223fbcfa6
Lang Cheng <lang.chlorine@gmail.com>
2019-05-27 16:13:51 -0700

    Updates

--- precache-manifest.db4f14a6b6501911e2a54e3822cd3a7f.js
+++ precache-manifest.db4f14a6b6501911e2a54e3822cd3a7f.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8fadb50c4219be06c6459874c7111fbd",
+    "revision": "7ef8a401559b7987d3af945d07cf7703",
     "url": "/Radiative-Capture-Calculator/index.html"
   },
   {
-    "revision": "84e15bc2ee7dd38f3d42",
+    "revision": "d6a8b180e6aacb387b69",
     "url": "/Radiative-Capture-Calculator/static/css/main.90456ef9.chunk.css"
   },
   {
-    "revision": "a60e2ebddaae9d356ac1",
-    "url": "/Radiative-Capture-Calculator/static/js/2.d780cf11.chunk.js"
+    "revision": "c8b7452514dc3f9eba23",
+    "url": "/Radiative-Capture-Calculator/static/js/2.ea7ddcc3.chunk.js"
   },
   {
-    "revision": "84e15bc2ee7dd38f3d42",
-    "url": "/Radiative-Capture-Calculator/static/js/main.0c239062.chunk.js"
+    "revision": "d6a8b180e6aacb387b69",
+    "url": "/Radiative-Capture-Calculator/static/js/main.d7244812.chunk.js"
   },
   {
     "revision": "e5e15a80d6417f5f71ce",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Radiative-Capture-Calculator/precache-manifest.5527783a5cf740de04bf5cf3e4c85c59.js"
+  "/Radiative-Capture-Calculator/precache-manifest.db4f14a6b6501911e2a54e3822cd3a7f.js"
 );
 
 self.addEventListener('message', (event) => {
